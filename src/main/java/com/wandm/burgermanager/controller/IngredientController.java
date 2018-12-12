36c4484422487e00bdfc60dc652a6e394be309e1
werari <weronikaarim@yahoo.com>
2018-12-12 20:07:38 +0100

    trying

--- src/main/java/com/wandm/burgermanager/controller/IngredientController.java
+++ src/main/java/com/wandm/burgermanager/controller/IngredientController.java
@@ -12,4 +12,10 @@ public class IngredientController {
         ModelAndView modelAndView = new ModelAndView("index");
         return modelAndView;
     }
+    @GetMapping("/hello")
+    public ModelAndView hello (){
+        ModelAndView modelAndView = new ModelAndView("Hello MgSzu");
+        return modelAndView;
+
+    }
 }
