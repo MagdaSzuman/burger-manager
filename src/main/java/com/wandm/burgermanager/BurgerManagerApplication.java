9fc803473d54da2e1c031fe0318ee834e2fde2cf
M <szumagda@wp.pl>
2018-12-12 18:55:53 +0100

    initial commit

---
+++ src/main/java/com/wandm/burgermanager/BurgerManagerApplication.java
@@ -0,0 +1,14 @@
+package com.wandm.burgermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerManagerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/wandm/burgermanager/BurgerManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wandm.burgermanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BurgerManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
